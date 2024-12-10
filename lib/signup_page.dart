c358832cbf4be0dd24f75964c57d4c4c1e605d0b
habibirfan180 <habibirfan180@gmail.com>
2024-12-10 20:11:47 +0530

    complete login page

--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class SignupPage extends StatefulWidget {
+  static route() => MaterialPageRoute(builder: (context) => SignupPage());
   const SignupPage({super.key});
 
   @override
@@ -10,7 +11,32 @@ class SignupPage extends StatefulWidget {
 
 class _SignupPageState extends State<SignupPage> {
   @override
+  final emailControler = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  void dispose() {
+    emailControler.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
-    return const Text("Irfan Habib");
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Sign Up.',
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
